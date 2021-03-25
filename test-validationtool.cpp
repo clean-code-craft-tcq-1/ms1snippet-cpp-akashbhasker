b80133d9f36dc44aed8cb2a78b77cd62bf57f1d3
ahk7kor <akash.bhasker@in.bosch.com>
2021-03-25 15:40:03 +0530

    Improves Test case description

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -7,7 +7,7 @@
 /*----------------------------------------------------------------------------------
  * SoC Sensor Tests
  */
-TEST_CASE("Report error when SoC readings jumps abruptly - Upward Spike")
+TEST_CASE("Reports ERROR when SoC readings JUMP ABRUPBTLY - Upward Spike")
 {
   double socReadings[] = {0.0, 0.01, 0.5, 0.51, 0.6};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
@@ -15,7 +15,7 @@ TEST_CASE("Report error when SoC readings jumps abruptly - Upward Spike")
   REQUIRE(isSOCReadingsValid(socReadings, numOfSocReadings) == false);
 }
 
-TEST_CASE("Reports error when SoC readings jumps abruptly - Downward Spike")
+TEST_CASE("Reports ERROR when SoC readings JUMP ABRUPBTLY - Downward Spike")
 {
   double socReadings[] = {0.51, 0.01, 0.5, 0.1};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
@@ -23,13 +23,22 @@ TEST_CASE("Reports error when SoC readings jumps abruptly - Downward Spike")
   REQUIRE(isSOCReadingsValid(socReadings, numOfSocReadings) == false);
 }
 
-TEST_CASE("Reports error when empty SOC readings is sent") {
+TEST_CASE("Reports ERROR when SoC readings are EMPTY")
+{
   double socReadings[] = {};
 
   REQUIRE(isSOCReadingsValid(socReadings, 0) == false);
 }
 
-TEST_CASE("reports no error when single SoC reading is passed")
+
+TEST_CASE("Reports ERROR when SoC Readings are passed as NULL")
+{
+
+  REQUIRE(isSOCReadingsValid(NULL, 0) == false);
+}
+
+
+TEST_CASE("Reports NO ERROR when ONLY ONE SoC reading is passed")
 {
   double socReadings[] = {0.01};
   int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
@@ -37,16 +46,20 @@ TEST_CASE("reports no error when single SoC reading is passed")
   REQUIRE(isSOCReadingsValid(socReadings, numOfSocReadings) == true);
 }
 
-TEST_CASE("reports error when NULL SoC data is sent") {
+TEST_CASE("Reports NO ERROR when VALID SoC readings are passed")
+{
+  double socReadings[] = {0.01, 0.02, 0.03, 0.04};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
 
-  REQUIRE(isSOCReadingsValid(NULL, 0) == false);
+  REQUIRE(isSOCReadingsValid(socReadings, numOfSocReadings) == true);
 }
+
 
 /*----------------------------------------------------------------------------------
  *  Current Sensor Tests
  */
 
-TEST_CASE("reports error when current jumps abruptly - Upward Spike")
+TEST_CASE("Reports ERROR when Current readings JUMP ABRUPBTLY - Upward Spike")
 {
   double currentReadings[] = {0.03, 0.03, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
@@ -54,7 +67,7 @@ TEST_CASE("reports error when current jumps abruptly - Upward Spike")
   REQUIRE(isCurrentReadingsValid(currentReadings, numOfCurReadings) == false);
 }
 
-TEST_CASE("reports error when current jumps abruptly - Downward Spike")
+TEST_CASE("Reports ERROR when Current readings JUMP ABRUPBTLY - Downward Spike")
 {
   double currentReadings[] = {0.33, 0.03, 0.03};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
@@ -62,14 +75,19 @@ TEST_CASE("reports error when current jumps abruptly - Downward Spike")
   REQUIRE(isCurrentReadingsValid(currentReadings, numOfCurReadings) == false);
 }
 
-TEST_CASE("reports error when empty current data is sent")
+TEST_CASE("Reports ERROR when Empty Current reading is passed")
 {
   double currentReadings[] = {};
 
   REQUIRE(isCurrentReadingsValid(currentReadings, 0) == false);
 }
 
-TEST_CASE("reports no error when single Current reading is passed")
+TEST_CASE("Reports ERROR when NULL Current reading is passed") {
+
+  REQUIRE(isCurrentReadingsValid(NULL, 0) == false);
+}
+
+TEST_CASE("Reports NO ERROR when ONLY ONE Current reading is passed")
 {
   double currentReadings[] = {0.03};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
@@ -77,10 +95,15 @@ TEST_CASE("reports no error when single Current reading is passed")
   REQUIRE(isCurrentReadingsValid(currentReadings, numOfCurReadings) == true);
 }
 
-TEST_CASE("reports error when NULL Current data is sent") {
+TEST_CASE("Reports NO ERROR when VALID Current readings are passed")
+{
+  double currentReadings[] = {0.03, 0.04, 0.05, 0.06};
+  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
 
-  REQUIRE(isCurrentReadingsValid(NULL, 0) == false);
+  REQUIRE(isCurrentReadingsValid(currentReadings, numOfCurReadings) == true);
 }
+
+
 
 
 
